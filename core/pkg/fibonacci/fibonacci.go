9c550f76eee3f914e894d1b678c3611d2c2a88c5
Joaquin Franco <chino.franco@gmail.com>
2023-12-15 19:10:32 +0900

    fix: set limit for uint64 output

--- core/pkg/fibonacci/fibonacci.go
+++ core/pkg/fibonacci/fibonacci.go
@@ -6,7 +6,7 @@ import (
 
 type FibonacciSequence struct {
 	Length   int
-	Sequence []int
+	Sequence []uint64
 }
 
 func fibonacciValue(number int) int {
@@ -22,19 +22,24 @@ func fibonacciValue(number int) int {
 }
 
 func Fibonacci(limit int) *FibonacciSequence {
-	result := []int{0}
+	result := []uint64{0}
+	hardCap := 93
 	if limit == 1 {
 		result = append(result, 1)
 	} else {
 		// Generate Fibonacci numbers up to the limit
-		result = []int{0, 1}
+		if limit >= hardCap {
+			log.Printf("Maximum limit is %d", hardCap)
+			limit = hardCap - 1
+		}
+		result = []uint64{0, 1}
 		for i := 2; i < limit; i++ {
 			nextFib := result[i-1] + result[i-2]
 			result = append(result, nextFib)
 		}
 	}
 
-	log.Printf("Generated Fibonacci number sequence for %d: %v", limit, result)
+	log.Printf("Generated Fibonacci number sequence for %d", limit)
 	return &FibonacciSequence{
 		Length:   len(result),
 		Sequence: result,
