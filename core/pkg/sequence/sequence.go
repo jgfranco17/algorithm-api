42234b04c3c0fc08011d879faf11771c99cf0000
Joaquin Franco <chino.franco@gmail.com>
2023-12-29 23:58:48 +0900

    chore: clean up comments

--- core/pkg/sequence/sequence.go
+++ core/pkg/sequence/sequence.go
@@ -3,14 +3,12 @@ package sequence
 func longestCommonSubsequence(mainString string, subString string) (string, error) {
 	m := len(mainString)
 	n := len(subString)
-
-	// Create a 2D slice to store the LCS lengths
 	lcsLengths := make([][]int, m+1)
+
 	for i := range lcsLengths {
 		lcsLengths[i] = make([]int, n+1)
 	}
 
-	// Build the LCS lengths matrix
 	for i := 0; i <= m; i++ {
 		for j := 0; j <= n; j++ {
 			if i == 0 || j == 0 {
@@ -23,9 +21,9 @@ func longestCommonSubsequence(mainString string, subString string) (string, erro
 		}
 	}
 
-	// Reconstruct the LCS from the matrix
 	var lcs string
 	i, j := m, n
+
 	for i > 0 && j > 0 {
 		if mainString[i-1] == subString[j-1] {
 			lcs = string(mainString[i-1]) + lcs
