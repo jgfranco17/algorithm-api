42fa3579cdaa460bec33152443225f356220b1a8
Joaquin Franco <chino.franco@gmail.com>
2024-02-27 23:27:45 +0900

    fix: add port for listening

--- service/cmd/main.go
+++ service/cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 
 	"github.com/gin-gonic/gin"
 	"github.com/jgfranco17/algorithm-api/service/pkg/env"
@@ -38,8 +39,9 @@ func main() {
 		log.Infof("Running API production server on port %d", *port)
 		gin.SetMode(gin.ReleaseMode)
 	}
-	err := router.GetRouter().Run()
+	listeningPort := fmt.Sprintf(":%d", *port)
+	err := router.GetRouter().Run(listeningPort)
 	if err != nil {
-		log.Error("Error starting the server:", err)
+		log.Errorf("Error starting the server: %v", err)
 	}
 }
